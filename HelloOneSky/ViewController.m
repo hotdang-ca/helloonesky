fcb990d933889d4066abf692215dbb4283df94d3
James Perih <james.perih@tippingcanoe.com>
2016-04-21 09:32:06 -0500

    Initial Commit

---
+++ HelloOneSky/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HelloOneSky
+//
+//  Created by James Perih on 2016-04-21.
+//  Copyright © 2016 Hot Dang Interactive. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ HelloOneSky/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloOneSky
+//
+//  Created by James Perih on 2016-04-21.
+//  Copyright © 2016 Hot Dang Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
